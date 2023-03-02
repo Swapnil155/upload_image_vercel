c03de07fa6ccf11fb2e19c78227d7ad65fd1b26c
Swapnil <swapnilbendal155@gmail.com>
2023-03-02 18:40:50 +0530

    change

--- app.js
+++ app.js
@@ -3,64 +3,66 @@ const express = require('express')
 const multer = require('multer')
 const path = require('path')
 const app = express()
-const fs =  require('fs')
+const fs = require('fs')
 
 
 app.use(json())
 
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Define the upload directory
 const UPLOAD_DIR = 'uploads'
 
 // Define the storage engine for multer
 const storage = multer.diskStorage({
-    destination: (req, file, callback) => {
-        
-      callback(null, UPLOAD_DIR)
-    },
-    filename: (req, file, callback) => {
-      const filename = `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
-      callback(null, filename)
+  destination: (req, file, callback) => {
+
+    callback(null, UPLOAD_DIR)
+  },
+  filename: (req, file, callback) => {
+    const filename = `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+    callback(null, filename)
+  }
+})
+
+// Create an instance of the multer middleware
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 10 // 10 MB
+  },
+  fileFilter: (req, file, callback) => {
+    const allowedTypes = ['image/jpeg', 'image/png', 'image/gif']
+    if (allowedTypes.includes(file.mimetype)) {
+      callback(null, true)
+    } else {
+      callback(new Error('Invalid file type. Only JPEG, PNG, and GIF files are allowed.'))
     }
-  })
-  
-  // Create an instance of the multer middleware
-  const upload = multer({
-    storage: storage,
-    limits: {
-      fileSize: 1024 * 1024 * 10 // 10 MB
-    },
-    fileFilter: (req, file, callback) => {
-      const allowedTypes = ['image/jpeg', 'image/png', 'image/gif']
-      if (allowedTypes.includes(file.mimetype)) {
-        callback(null, true)
-      } else {
-        callback(new Error('Invalid file type. Only JPEG, PNG, and GIF files are allowed.'))
-      }
-    }
-  })
-  
-  // Define the route handler for uploading files
-  app.post('/upload', upload.single('file'), (req, res) => {
-    try {
-      // Get the file from the request body
-      const file = req.file
-  
-      // Construct the URL of the uploaded file
-      const fileUrl = `${process.env.VERCEL_URL}/${UPLOAD_DIR}/${file.filename}`
-  
-      // Return the URL of the uploaded file
-      res.status(200).json({ url: fileUrl, file :  file })
-    } catch (error) {
-      console.error(error)
-      res.status(500).json({ message: 'Error uploading file',
-    error : error.message  })
-    }
-  })
-  
-  // Start the server
-  app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server started on port ${process.env.PORT || 3000}`)
-  })
+  }
+})
+
+// Define the route handler for uploading files
+app.post('/upload', upload.single('file'), (req, res) => {
+  try {
+    // Get the file from the request body
+    const file = req.file
+
+    // Construct the URL of the uploaded file
+    const fileUrl = `${process.env.VERCEL_URL}/${UPLOAD_DIR}/${file.filename}`
+
+    // Return the URL of the uploaded file
+    res.status(200).json({ url: fileUrl, file: file })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({
+      message: 'Error uploading file',
+      error: error.message
+    })
+  }
+})
+
+// Start the server
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server started on port ${process.env.PORT || 3000}`)
+})
 
